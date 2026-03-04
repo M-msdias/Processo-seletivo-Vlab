079a7abc741bf0f9a9550619f1de9c567d601fd4
Marcela Dias <ddiasmarcela@gmail.com>
2026-03-04 08:12:31 -0300

    chore: add fileReplacements for production environment config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api' // trocar pela URL de produção futuramente
+  apiUrl: 'https://processo-seletivo-vlab-1.onrender.com/api'
 };
